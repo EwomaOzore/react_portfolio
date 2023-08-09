52d3120786d879fdfa76cc1bf719b418ced1e02f
Ewoma Ozore <ewomaozore@gmail.com>
2023-08-09 18:50:51 +0100

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import img from '../images/virtual-reality.png';
 
 const Header = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const handleMobileMenuToggle = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    };
+
     return (
         <div id="header" className="header-container">
             <div className="container">
                 <nav>
                     <h1>Ewoma</h1>
-                    <ul>
+                    <FontAwesomeIcon
+                        className={`mobile-menu-icon ${mobileMenuOpen ? 'active' : ''}`}
+                        icon={mobileMenuOpen ? faTimes : faBars}
+                        onClick={handleMobileMenuToggle}
+                    />
+                    <ul className={`nav-list ${mobileMenuOpen ? 'mobile-open' : ''}`}>
                         <li><a href="#header">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#certifications">Certifications</a></li>
                         <li><a href="#Projects">Projects</a></li>
                         <li><a href="#contact">Contact</a></li>
-                        <FontAwesomeIcon className='fas' icon={faXmark} />
                     </ul>
-                    <FontAwesomeIcon className='fas' icon={faBars} />
+                    <div className={`mobile-overlay ${mobileMenuOpen ? 'open' : ''}`} onClick={handleMobileMenuToggle}></div>
                 </nav>
                 <div className="container1">
                     <div className="header-text">
