8487809715506dcf1df0c7aacc605d50ac697021
Ewoma Ozore <ewomaozore@gmail.com>
2023-09-06 08:55:24 +0100

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,6 @@ const About = () => {
         <div id="about">
             <div className="container">
                 <h1 className="sub-title">About Me</h1>
-                <div className="about-col-1">
-
-                </div>
                 <div className='about-col-2'>
                     <p>
                         I am an enthusiastic and dedicated professional with a passion for continuous learning and growth. I am eager to take on new challenges and broaden my horizons. I am highly motivated to continue growing professionally, whether through pursuing advanced certifications, attending industry conferences, or taking on increasingly complex projects. I strive to stay at the forefront of emerging trends and technologies, as I believe that continuous learning is essential for personal and organizational success.
